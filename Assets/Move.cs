0c7812b3b904b29e13853ddaf3ae16e15cdb6b7c
0507jiho <0507jiho@users.noreply.github.com>
2017-11-04 16:48:56 +0900

    달리기, 중력, 점프(구현 중)

---
+++ Assets/Move.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 10f;
+    public const float moveSpeed = 5.0f;
+    public float jumpSpeed = 10f;
+    private Rigidbody2D rigid;
+
+    Vector3 movement;
+    bool isJumping = false;
+
+    private void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+        else isJumping = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Jumpcontrol();
+        MoveControl();
+
+    }
+
+    void MoveControl()
+    {
+        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        this.gameObject.transform.Translate(distanceX, 0, 0);
+
+    }
+
+    void Jumpcontrol ()
+    {
+        if (!isJumping) return;
+
+        else
+        {
+            rigid.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpSpeed);
+            rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+        }
+        
+
+    }
+
+}
