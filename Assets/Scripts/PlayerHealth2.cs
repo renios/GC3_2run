e616a501e4d046686751a4d37f00b86599b9f870
networkCHL <networkCHL@users.noreply.github.com>
2017-11-18 16:46:05 +0900

    조금수정

--- Assets/Scripts/PlayerHealth2.cs
+++ Assets/Scripts/PlayerHealth2.cs
@@ -23,17 +23,17 @@ public class PlayerHealth2 : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-<<<<<<< HEAD
+
         if (other.gameObject.tag == "monster")
             player2Health = player2Health - 1;
         if (other.gameObject.tag == "big_Monster")
             player2Health = 0;
         if (other.gameObject.tag == "Heart")
             player2Health = player2Health++;
-=======
+
         if (other.gameObject.tag == "monster") player2Health = player2Health - 1;
         if (other.gameObject.tag == "big_Monster") player2Health = 0;
         if (other.gameObject.tag == "spikes") player2Health = player2Health - 1;
->>>>>>> cb64176298005b50d24b4fb3f7dcc8a79e8ea7a9
+
     }
 }
